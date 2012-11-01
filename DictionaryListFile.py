4361f67b234f64e66d4a942df95c2c1cb0f52579
ajcagalawan <james.cagalawan@uwaterloo.ca>
2012-11-01 17:12:18 -0400

    Renamed files for more clarity and added more comments. Added readme.

--- DictionaryListFile.py
+++ DictionaryListFile.py
@@ -2,12 +2,20 @@ import urllib2
 from sys import argv
 from Tkinter import *
 import tkFileDialog
-import tkSimpleDialog
+
+#This version takes words from a file input
 
 def getDefinition(wordArg):
+    """
+    Returns the definition of the word wordArg.
+    """
+
+    #Get the page from an online dictionary
     req = urllib2.Request('http://dictionary.reference.com/browse/' + wordArg)
     response = urllib2.urlopen(req)
     the_page = response.read()
+
+    //find the word in the page
     if(the_page.find('td3n2') != -1):
         definitionIndex = the_page.find('td3n2')
         definitionIndex = the_page.find('>', definitionIndex) + 1
@@ -19,29 +27,34 @@ def getDefinition(wordArg):
         target.write(str(n) + "." + wordArg + ": Error finding definition.")
 
 def cleanup(defString):
+    """
+    Cleans up HTML formatting from the definition
+    """
     while(defString.find('<') != -1):
         index1 = defString.find('<')
         index2 = defString.find('>', index1) + 1
         defString = defString[:index1] + defString[index2:]
     return defString
 
-running = True
+
 #script, inputfile, outputfile = argv
 #make GUI
 root = Tk()
-w = Label(root, text = "Enter words. When you are done, enter nothing and press cancel.")
+w = Label(root, text = "Select the input file and then the output file")
 w.pack()
 
-outputfile = tkFileDialog.asksaveasfilename(title = 'Choose file to save definitions',filetypes = [('Text File', '*.txt')], initialfile = 'DefinitionList.txt')
+print ("Select the input file")
+inputfile = tkFileDialog.askopenfilename()
+print ("Select the output file")
+outputfile = tkFileDialog.askopenfilename()
+txt = open(inputfile)
 target = open(outputfile, 'w')
+target.truncate()
 n = 1
 
-while running:
-    word = tkSimpleDialog.askstring(title = 'Enter Word',prompt = 'Enter the word which you want to define')
-    if (len(word) == 0):
-        running = False
-    else:
-        getDefinition(word)
-        n = n + 1
+for word in txt:
+    realWord = word[:len(word) - 1]
+    getDefinition(realWord)
+    n = n + 1
 
 target.close()
